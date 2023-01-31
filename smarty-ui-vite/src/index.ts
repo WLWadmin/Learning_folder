02dcff0d5470b98edcb26d8e56eb17aae0f39209
wulingwei <wulingwei@bytedance.com>
2023-01-31 14:15:34 +0800

    Second submission

--- smarty-ui-vite/src/index.ts
+++ smarty-ui-vite/src/index.ts
@@ -2,6 +2,7 @@ const s: string = 'Hello TypeScript'
 console.log(s)
 
 import {createApp} from 'vue';
-import SButton from './button';
+// import SButton from './button';
+import SFCButton from './SFCButton.vue';
 
-createApp(SButton).mount('#app');
+createApp(SFCButton).mount('#app');
